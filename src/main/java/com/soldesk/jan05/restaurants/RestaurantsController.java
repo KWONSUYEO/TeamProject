984dcbe360b2f3ee79a57f7fa2f16237f933ebd5
skolo <skolo@LAPTOP-O7VNBH0A>
2023-01-20 09:59:52 +0900

    0120_GH_1

--- src/main/java/com/soldesk/jan05/restaurants/RestaurantsController.java
+++ src/main/java/com/soldesk/jan05/restaurants/RestaurantsController.java
@@ -7,16 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.soldesk.jan05.member.MemberDAO;
+
 @Controller
 public class RestaurantsController {
-	
+
+	@Autowired
+	private MemberDAO mDAO;
 	@Autowired
 	private RestaurantsDAO rDAO;
 	
 	@RequestMapping(value = "/dibs.go", method = RequestMethod.GET)
 	public String info(HttpServletRequest req) {
-		rDAO.getAllDibs(req);
-		req.setAttribute("main", "restaurants/dibsList.jsp");
+		if (mDAO.loginCheck(req)) {
+			rDAO.getAllDibs(req);
+			req.setAttribute("main", "restaurants/dibsList.jsp");			
+		} else {
+			req.setAttribute("main", "home.jsp");
+		}
 		return "index";
 	}
 }
--- target/classes/db.sql
+++ target/classes/db.sql
@@ -2,14 +2,14 @@ create table member(
 	m_id varchar2(10 char) primary key, -- id
 	m_password varchar2(20 char) not null, -- pw
 	m_nickname varchar2(10 char) not null, -- 닉네임
-	m_category varchar2(15 char), -- 선호하는 카테고리
-	m_email varchar2(30 char)
+	m_location varchar2(30 char) not null -- 지역
 )
+
 select * from member
 
 drop table member
 
-insert into MEMBER values('user_1', '1', 'nick_1', '한식', 'user1@gmail.com')
+insert into MEMBER values('user_1','1','kwon','경기도')
 
 -- 맛집리스트(찜/방문 겸용)
 create table restaurants(
@@ -41,19 +41,4 @@ select * from restaurants where r_level = 1
 
 
 
-
-
-
-create table member(
-	m_id varchar2(10 char) primary key, -- id
-	m_password varchar2(20 char) not null, -- pw
-	m_nickname varchar2(10 char) not null, -- 닉네임
-	m_location varchar2(30 char) not null -- 지역
-)
-
-select * from member
-
-drop table member
-
-insert into MEMBER values('ksy','1','kwon','경기도')
 
